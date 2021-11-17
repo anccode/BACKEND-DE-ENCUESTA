8004186cb45d46fbb45e21a85e58a75b8de4bb4b
josngel <cj_angel@outlook.com>
2021-11-17 12:51:25 -0500

    password comparison to login

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
  module.exports = {
-    mogoUrl:"", //you data base 
+    mogoUrl:"mongodb+srv://admin:1531angel@cluster0.ibyni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", //you data base 
     jwtkey: "asdasdas"
  }
