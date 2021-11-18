a546ef75b8e3af264edd5c7be9cc58fadbc9e1ee
josngel <cj_angel@outlook.com>
2021-11-18 02:59:38 -0500

    create api

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 
-const mongoose = require('mongoose');
+require('./models/db');
+const User = require('./models/user');
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-}).then(()=>{
-    console.log('yeahh database correct')
-}).catch(err=> console.log(err.message))
+const app = express();
 
-const app = express()
+email = 'angel2';
+
+app.post('/create-user', async(req,res) => {
+    const isNewUser = await User.isThisEmailInUse(email);
+    if(!isNewUser) 
+        return res.json({
+            success:false, 
+            message:'this email is already in use, try sign-in'
+    });
+
+    const user = await User({
+        fullname: '    ', 
+        email:email,
+        password:'    '
+    });
+    await user.save();
+    res.json(user);
+})
 
 app.get('/', (req,res)=>{
-    
+    res.send('<h1>Hola</h1>')
 } )
 
-app.listen(8000,()=>{
+app.listen(3000,()=>{
     console.log('port in listening');
 })
--- models/user.js
+++ models/user.js
@@ -17,10 +17,25 @@ const userSchema = new mongoose.Schema({
     email:{
         type: String,
         requierd: true,
+        unique:true
     },
     password:{
         type: String,
         requierd: true
     },
-    avatar: buffer,
+    avatar: Buffer,
 })
+
+userSchema.statics.isThisEmailInUse = async function(email){
+    if(!email) throw new Error('Invalid email');
+    try {
+        const user = await this.findOne({ email });
+        if (user) return false
+        return true;
+    } catch (error) {
+        console.log('error inside isThisEmailInUse method', error.message);
+        return false;   
+    }   
+};
+
+module.exports = mongoose.model('User', userSchema)
