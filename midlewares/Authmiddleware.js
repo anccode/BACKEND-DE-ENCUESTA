d9404f6fbda7b197617711ada44c7a9221ea1c58
josngel <cj_angel@outlook.com>
2021-12-08 16:22:40 -0500

    agregando comentarios y eliminando comentarios por id

--- midlewares/Authmiddleware.js
+++ midlewares/Authmiddleware.js
@@ -5,8 +5,11 @@ const validateToken = (req,res, next)=>{
 
     if(!accessToken) return res.json({error: "user no logged"});
     try {
+        
         const validToken = verify(accessToken, "importantsecret");
-        if(validToken){ 
+        req.user = validToken;
+        
+        if(validToken){   
             return next();
         }
     } catch (err) {
--- routes/Users.js
+++ routes/Users.js
@@ -1,39 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Users} = require('../models');
-const bcrypt = require("bcrypt");   
-const { sign } = require('jsonwebtoken');
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+const {validateToken} = require('../midlewares/Authmiddleware')
 
-
-router.post("/", async (req,res) => {
-    const {username,password} = req.body; 
-    bcrypt.hash(password,10).then((hash)=>{
-        Users.create({
-            username: username,
-            password: hash,
-        });
-        res.json("SUCCESS");
-    }); 
-});
-
-router.post("/login", async(req,res)=>{
-
-    const {username, password} = req.body;  
-    const user = await Users.findOne({ where: {username: username } });
-
-    if(!user) res.json({error: "usuario no existe"});
-
-    bcrypt.compare(password, user.password).then((match)=>{
-
-        if(!match) res.json({error: "tu usuario y tu contraseña es incorrecto"});
-
-        const accessToken = sign({username: user.username, id: user.id },
-             "importantsecret"
-             )
-
-        res.json(accessToken);
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
     });
-
+    res.json("SUCCESS");
+  });
 });
 
-module.exports = router
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) res.json({ error: "usuario no existe" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) res.json({ error: "tu usuario y tu contraseña es incorrecto" });
+
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+
+    res.json({token: accessToken, username: username, id: user.id });
+  });
+});
+
+
+router.get ('/auth', validateToken, (req,res)=>{
+  res.json(req.user);
+})
+
+module.exports = router;
+
